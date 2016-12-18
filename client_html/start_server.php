e59b682d82190bd6c2b1736b890054feba8f8556
Beni Yager <jaeggiben@gmail.com>
2016-12-18 06:13:32 +0000

    Automatic start of server if offline and request for admin page. *Failsafe*, cannot be stopped after, must be rebooted.

--- client_html/start_server.php
+++ client_html/start_server.php
@@ -1,8 +1,11 @@
 <?php
+echo "[Failsafe] Trying to start the server...";
 $result2=exec("/home/pi/Documents/ThereWillBeLED/autoboot/client/client_autostart", $output, $retval);
 echo "Result: $result<br />";
 foreach($output as $o)
 	echo "$o<br />";
 echo "Retval: $retval<br />";
-echo "DONE &lt;3<br />";
+echo "DONE &lt;3<br /><br />";
+echo "<a href='index.html'>Go To Main Page</a>";
+
 ?>
